7044fbf1298438919af6343e5d863dace6e2962e
Randunu Akila <akilamadhufin@outlook.com>
2025-10-07 20:06:07 +0300

    Adding fetch donations function

--- App.js
+++ App.js
@@ -3,11 +3,53 @@ import { StyleSheet, Text, View, TextInput,TouchableOpacity,Image,ScrollView,Act
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+// categories array
+const categories = ['All', 'Clothes', 'Books', 'Sports', 'Electronics'];
+const SERVER_URL = 'http://10.0.2.2:3000'; // server URL
+
 export default function App() {
 
 // useState for authentication
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
+
+// useState for items
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [search, setSearch] = useState('');
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
+
+    // Fetch donations from server
+  const fetchDonations = async (isRefresh = false) => {
+    try {
+      if (!isRefresh) setLoading(true);
+      const response = await fetch(`${SERVER_URL}/api/donations`);
+      console.log('Response status:', response.status);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      const donations = data.data;
+      
+      console.log('Using donations array:', donations.length, 'items');
+      
+      setItems(donations);
+      setError(null);
+    } catch (err) {
+      console.error('Error fetching donations:', err);
+      setError('Failed to load donations. Please check your internet connection.');
+    } finally {
+      if (isRefresh) {
+        setRefreshing(false);
+      } else {
+        setLoading(false);
+      }
+    }
+  };
+
 
 
   return (
