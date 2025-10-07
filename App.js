4965e7a960f7d4e25c9bd6c82e9e271096a93ad0
Randunu Akila <akilamadhufin@outlook.com>
2025-10-07 19:49:50 +0300

    adding useState isLoggedIn, user

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
 export default function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+
   return (
     <View style={styles.container}>
       <Text>this is let's donate app</Text>
