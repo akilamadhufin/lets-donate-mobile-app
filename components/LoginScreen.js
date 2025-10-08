6a12bed235f2d54c8c912c1a819f295f8fc81f14
GimhaniKaushalya <126944401+GimhaniKaushalya@users.noreply.github.com>
2025-10-08 18:24:50 +0300

    UI header

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -13,10 +13,19 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const LoginScreen = ({ onLogin, onSignUp, loading, error }) => {
   const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    
+
     const handleLoginPress = () => {
         if (email && password) {
           onLogin(email, password);
         }
       };
-    
+    return (
+        <SafeAreaView style={styles.loginContainer}>
+          <View style={styles.loginContent}>
+           {/* Header */}
+                   <View style={styles.loginHeader}>
+                     <TouchableOpacity style={styles.backButton}>
+                       <Text style={styles.backArrow}>←</Text>
+                     </TouchableOpacity>
+                     <Text style={styles.loginTitle}>Login</Text>
+                   </View>
