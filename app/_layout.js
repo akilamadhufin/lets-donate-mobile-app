8745ede48b42c676b80df16c2e6e8742f4c69094
Randunu Akila <akilamadhufin@outlook.com>
2025-10-10 11:42:54 +0300

    creating index.js, login.js, homescreen.js files

--- app/_layout.js
+++ app/_layout.js
@@ -3,7 +3,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{headerTitle: "Main page", headerShown: true, headerLeft:()=><></> }} />
-      <Stack.Screen name="donate" options={{headerTitle: "Test Form", headerShown: true }} />
+      {/* <Stack.Screen name="donate" options={{headerTitle: "Test Form", headerShown: true }} /> */}
     </Stack>
   );
 }
