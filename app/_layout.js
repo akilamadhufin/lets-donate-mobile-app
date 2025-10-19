171fff4b86454c6b2db4b7e91f8dd6a9d48af254
Randunu Akila <akilamadhufin@outlook.com>
2025-10-19 08:49:50 +0300

    stack navigation roots

--- app/_layout.js
+++ app/_layout.js
@@ -4,6 +4,13 @@ export default function RootLayout() {
     <Stack initialRouteName="login">
       <Stack.Screen name="login" options={{ headerTitle: "Login", headerShown: true }} />
       <Stack.Screen name="index" options={{ headerTitle: "Home", headerShown: true }} />
+      <Stack.Screen name="donate" options={{ headerTitle: "Donate", headerShown: true }} />
+      <Stack.Screen name="basket" options={{ headerTitle: "My Basket", headerShown: true }} />
+      <Stack.Screen name="profile" options={{ headerTitle: "Profile", headerShown: true }} />
+      <Stack.Screen name="mydonations" options={{ headerTitle: "My Donations", headerShown: true }} />
+      <Stack.Screen name="myaccount" options={{ headerTitle: "My Account", headerShown: true }} />
+      <Stack.Screen name="updateaccount" options={{ headerTitle: "Update Account", headerShown: true }} />
+      <Stack.Screen name="editdonation" options={{ headerTitle: "Edit Donation", headerShown: true }} />
     </Stack>
   );
 }
